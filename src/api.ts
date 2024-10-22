efdf3159f9025d1f050086fc8b3528d458b56036
jinkyeongk <jinkyeongk@naver.com>
2024-10-22 23:30:18 +0900

    api 보안 항목 파일 수정

---
+++ src/api.ts
@@ -0,0 +1,28 @@
+
+import { atom } from "recoil";
+
+
+const BASE_PATH = "https://api.themoviedb.org/3";
+
+const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: import.meta.env.VITE_AUTH_KEY
+    }
+  };
+  
+
+
+
+export function getMovies(){
+    return fetch(`${BASE_PATH}/movie/now_playing?language=ko-KR&page=1&region=KR`, options)
+        .then((response) => response.json()
+    );
+}
+export function searchEngine(keyword:string){
+  return fetch(`${BASE_PATH}/search/keyword?query=${keyword}&page=1` , options)
+    .then(response => response.json()
+
+  );
+}
