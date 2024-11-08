26242a190e645c048ef0012fc3559e07e13bd305
jinkyeongk <jinkyeongk@naver.com>
2024-11-08 22:23:43 +0900

    스크롤 컨트롤 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,30 @@
-import {  Switch, Route, BrowserRouter as Router} from "react-router-dom";
+import {  Switch, Route, BrowserRouter as Router, useLocation} from "react-router-dom";
 import Header from "./Components/Header";
 import Home from "./Routes/Home";
 import Tv from "./Routes/Tv";
 import Search from "./Routes/Search";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { RecoilRoot } from 'recoil';
+import { useEffect } from 'react';
 
 function App() {
   const client = new QueryClient();
+  const ScrollToTop = () => {
+    const location = useLocation();  // 현재 URL 위치를 추적하는 훅
+  
+    useEffect(() => {
+      window.scrollTo(0, 0);  // 페이지 이동 시 스크롤을 맨 위로 이동
+    }, [location]);  // location이 변경될 때마다 실행
+  
+    return null;
+  };
   
   return (
 
     <RecoilRoot>
     <QueryClientProvider client={client}>
-    <Router  basename="/netflix-react-app">
+    <Router  basename="/netflix-react-app" >
+    <ScrollToTop/>
       <Header/>
       <Switch>
         <Route path="/tv" >
